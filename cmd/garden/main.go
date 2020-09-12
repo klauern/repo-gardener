196d289bcbcf5ee3c2d280f53815da0db8a30f77
Nick Klauer <nklauer@zendesk.com>
2020-09-11 19:31:06 -0500

    ✨ add stub CLI calls

--- cmd/garden/main.go
+++ cmd/garden/main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"os"
 
+	"github.com/rs/zerolog/log"
 	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	app := &cli.App{}
+	app := &cli.App{
+		Name: "repo-garden",
+		Commands: []*cli.Command{
+			{
+				Name:  "init",
+				Usage: "initialize a new configuration to run tasks across repositories",
+			},
+		},
+	}
 
 	err := app.Run(os.Args)
 	if err != nil {
-		panic(err)
+		log.Fatal().Err(err).Msg("running app")
 	}
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/klauern/repo-gardener
 go 1.15
 
 require (
-	github.com/go-git/go-git/v5 v5.1.0 // indirect
-	github.com/rs/zerolog v1.19.0 // indirect
-	github.com/urfave/cli/v2 v2.2.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
+	github.com/rs/zerolog v1.19.0
+	github.com/urfave/cli/v2 v2.2.0
 )
